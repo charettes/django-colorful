748e5428b07065d050a5a093a36e70174a9b3c5e
Simon Charette <charette.s@gmail.com>
2014-06-01 11:16:01 -0400

    Fixed Py3.2 comptability.

--- colorful/widgets.py
+++ colorful/widgets.py
@@ -1,33 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.conf import settings
 from django.forms.widgets import TextInput
-try:
-    from django.utils.safestring import SafeUnicode
-except ImportError:
-    from django.utils.safestring import SafeText as SafeUnicode
-
-try:
-    url = settings.STATIC_URL
-except AttributeError:
-    try:
-        url = settings.MEDIA_URL
-    except AttributeError:
-        url = ''
+from django.utils.safestring import mark_safe
 
 
 class ColorFieldWidget(TextInput):
     class Media:
         css = {
-            'all': ("%scolorful/colorPicker.css" % url, )
+            'all': ("%scolorful/colorPicker.css",)
         }
-        js = ("%scolorful/jQuery.colorPicker.js" % url, )
+        js = ("%scolorful/jQuery.colorPicker.js",)
 
     input_type = 'color'
 
     def render_script(self, id):
-        return u'''<script type="text/javascript">
+        return '''<script type="text/javascript">
                     (function($){
                         $(document).ready(function(){
                             $('#%s').each(function(i, elm){
@@ -43,4 +31,4 @@ class ColorFieldWidget(TextInput):
         if not 'id' in attrs:
             attrs['id'] = "id_%s" % name
         render = super(ColorFieldWidget, self).render(name, value, attrs)
-        return SafeUnicode(u"%s%s" % (render, self.render_script(attrs['id'])))
+        return mark_safe("%s%s" % (render, self.render_script(attrs['id'])))
