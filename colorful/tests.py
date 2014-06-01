95a53f8757810ace7220f6e7f27c73b058499ea8
JV Clemence <james+github@jvc26.org>
2014-06-01 15:34:48 +0100

    Further code-review changes from @charettes - alphabetise django.* imports

--- colorful/tests.py
+++ colorful/tests.py
@@ -1,6 +1,6 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.test import TestCase
-from django.core.exceptions import ValidationError
 
 from .widgets import ColorFieldWidget
 from .fields import RGBColorField
