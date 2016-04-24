f249a997a4afe8e3b3be3fa6f01079e387ed8ab3
Simon Charette <charette.s@gmail.com>
2016-04-23 20:01:09 -0400

    Bumped version number to 1.2.0.

--- colorful/__init__.py
+++ colorful/__init__.py
@@ -4,6 +4,6 @@ from django.utils import version
 
 __all__ = ['VERSION', '__version__']
 
-VERSION = (1, 2, 0, 'alpha', 0)
+VERSION = (1, 2, 0, 'final', 0)
 
 __version__ = version.get_version(VERSION)
