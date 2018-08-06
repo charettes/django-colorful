a5a6c91fb8be3262b8d5ed5a3713d413818e9040
Shaun Mishler <shaun.mishler@wspdigital.com>
2018-08-06 10:10:49 +1000

    Make widget render function compatible with Django 2.1.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         'Environment :: Web Environment',
         'Framework :: Django',
         'Framework :: Django :: 1.11',
-        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
